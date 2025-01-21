44681b503c4666036b90d9f3da7a8876b43eaffd
constantijn <cdemeer@fim.uni-passau.de>
2025-01-21 01:57:47 +0100

    example

---
+++ example.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define GLOHOT_IMPLEMENTATION
+#include "glohot.h"
+#define GLOHOT_KEY_CODES
+#include "glohot_utils.h"
+
+void test(Glohot *glohot)
+{
+    (void*) glohot;
+    printf("Hotkey pressed!\n");
+}
+
+void stop(Glohot *glohot)
+{
+    Glohot_exit(glohot);
+}
+
+int main(void)
+{
+    Glohot glohot = Glohot_create(0, 0);
+    
+    Glohot_add(&glohot, GH_MOD_CTRL | GH_MOD_ALT, GH_VK_RETURN, &test);
+    Glohot_add(&glohot, GH_MOD_CTRL | GH_MOD_ALT, GH_VK_ESCAPE, &stop);
+    
+    if (Glohot_register(&glohot) != 0) return 1;
+    Glohot_listen(&glohot);
+    return 0;
+}
